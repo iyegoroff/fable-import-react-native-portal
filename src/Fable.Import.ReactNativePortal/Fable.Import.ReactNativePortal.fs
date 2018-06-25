cf9f19a95fe0223cfe4a2f65a862e7b1f90fbcee
Igor Yegorov <iegoroff@gmail.com>
2018-06-25 05:13:16 +0300

    0.0.6

--- src/Fable.Import.ReactNativePortal/Fable.Import.ReactNativePortal.fs
+++ src/Fable.Import.ReactNativePortal/Fable.Import.ReactNativePortal.fs
@@ -1,17 +1,18 @@
 [<RequireQualifiedAccess>]
 module Fable.Import.ReactNativePortal
 
+open Fable.Import.React
 open Fable.Core.JsInterop
 
 module R = Fable.Helpers.React
 
-let inline blackPortal (name: string) (children: React.ReactElement list): React.ReactElement =
+let inline blackPortal (name: string) (children: ReactElement list): ReactElement =
   R.createElement(import "BlackPortal" "react-native-portal", createObj ["name" ==> name], children)
 
-let inline whitePortal (name: string) (children: React.ReactElement list): React.ReactElement =
+let inline whitePortal (name: string) (children: ReactElement list): ReactElement =
   R.createElement(import "WhitePortal" "react-native-portal", createObj ["name" ==> name], children)
 
-let inline portalProvider (children: React.ReactElement list): React.ReactElement =
+let inline portalProvider (children: ReactElement list): ReactElement =
   R.createElement(import "PortalProvider" "react-native-portal", createEmpty, children)
 
 let inline enterPortal<'a> = blackPortal
